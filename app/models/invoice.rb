03856532889da3e947cc30405e2469d817788e85
danembb <dbrophy720@gmail.com>
2021-10-26 14:29:03 -0600

    Finalize validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,7 @@
 class Invoice < ApplicationRecord
+  validates :customer_id, presence: true
+  validates :merchant_id, presence: true
+  validates :status, presence: true
   belongs_to :merchant
   belongs_to :customer
   has_many :transactions

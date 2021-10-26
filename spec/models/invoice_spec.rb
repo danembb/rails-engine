00c86ccfc5fae99c282413827a30ddb2473619fb
danembb <dbrophy720@gmail.com>
2021-10-26 11:35:51 -0600

    Add relationships to models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Invoice, type: :model do
   describe 'relationships' do
     it { should belong_to(:merchant) }
     it { should belong_to(:customer) }
-    it { should have_many(:invoices) }
     it { should have_many(:transactions) }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Item, type: :model do
     it { should belong_to(:merchant) }
     it { should have_many(:invoice_items) }
   end
+
+  describe 'validations' do
+  end
 end

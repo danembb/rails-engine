d7df3d58b697dc6b2a056292c1427d2a2cc3338f
danembb <dbrophy720@gmail.com>
2021-10-26 11:24:08 -0600

    Create models and relationships

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Invoice, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:merchant) }
+    it { should belong_to(:customer) }
+    it { should have_many(:invoices) }
+    it { should have_many(:transactions) }
+  end
+end
